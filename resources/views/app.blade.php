b75e8f76dc7178b80652ddfd4ef022c68119fb61
AmjidJaved <amjid.javed@cartlow.com>
2024-08-10 22:17:32 +0500

    laravel 11 with breeze react setup

---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Scripts -->
+        @routes
+        @viteReactRefresh
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+        @inertiaHead
+    </head>
+
+    <body
+            x-data="{ page: 'signin', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }"
+            x-init="
+              darkMode = JSON.parse(localStorage.getItem('darkMode'));
+              $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+            :class="{'dark text-bodydark bg-boxdark-2': darkMode === true}"
+    >
+        @inertia
+    </body>
+</html>
