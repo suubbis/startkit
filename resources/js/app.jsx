7a10e7b423ca5980207915ba5c06855728878ba8
AmjidJaved <amjid.javed@cartlow.com>
2024-08-10 22:42:35 +0500

    integration of html template to reactjs

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,11 @@
 import './bootstrap';
 import '../css/app.css';
 
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '../css/style.css';
+import '../css/satoshi.css';
+
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
@@ -13,7 +18,13 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <React.StrictMode>
+                <Router>
+                    <App {...props} />
+                </Router>
+            </React.StrictMode>
+        );
     },
     progress: {
         color: '#4B5563',
