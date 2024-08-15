83fe890eb0e9bdbcf6f93ecd6493e05e068de348
AmjidJaved <amjid.javed@cartlow.com>
2024-08-16 00:05:05 +0500

    react guest layout setup and login page setup

--- resources/js/layouts/GuestLayout.tsx
+++ resources/js/layouts/GuestLayout.tsx
@@ -3,28 +3,12 @@ import React from 'react';
 const GuestLayout = ({ children }) => {
 
     return (
-        <div className="light:bg-boxdark-2 light:text-bodydark">
-            {/* <!-- ===== Page Wrapper Start ===== --> */}
-            <div className="flex h-screen overflow-hidden">
-                {/* <!-- ===== Sidebar Start ===== --> */}
-                {/*<Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />*/}
-                {/* <!-- ===== Sidebar End ===== --> */}
-
-                {/* <!-- ===== Content Area Start ===== --> */}
-                <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-                    {/* <!-- ===== Header End ===== --> */}
-
-                    {/* <!-- ===== Main Content Start ===== --> */}
-                    <main>
-                        <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                            {children}
-                        </div>
-                    </main>
-                    {/* <!-- ===== Main Content End ===== --> */}
-                </div>
-                {/* <!-- ===== Content Area End ===== --> */}
+        <div className="bg-white px-4 dark:bg-boxdark-2 sm:px-6">
+            <div className="flex h-screen flex-col overflow-hidden">
+                <main>
+                    {children}
+                </main>
             </div>
-            {/* <!-- ===== Page Wrapper End ===== --> */}
         </div>
     );
 };
